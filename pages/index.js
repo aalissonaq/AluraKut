728682a5c63416af5cd2dc993895b504add163aa
aalissonaq <aalissonalmeidaq@gmail.com>
2021-07-13 21:35:11 -0300

    Limpando formularios depois da  criação da comunicadade

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home() {
           </Box>
           <Box>
             <h2 className="subTitle">O que você deseja fazer?</h2>
-            <form
+            <form id="formCommunity"
               onSubmit={function handleCreateCommunity(e) {
                 e.preventDefault();
                 const dataForm = new FormData(e.target);
@@ -87,7 +87,7 @@ export default function Home() {
                 }
                 const communityUpdate = [...community, newCommunity];
                 setCommunity(communityUpdate);
-
+                document.getElementById('formCommunity').reset();
 
               }}>
               <div>
